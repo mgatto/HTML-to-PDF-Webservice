e19fcae1e2ed0fab5b3a63f2fc7c740657f825a4
Michael Gatto <mgatto@vinsuite.com>
2022-02-20 20:43:19 -0700

    feat: echo raw html from POST body to console.

--- src/main/java/edu/oregonstate/gattom/PdfGeneratorService.java
+++ src/main/java/edu/oregonstate/gattom/PdfGeneratorService.java
@@ -7,5 +7,16 @@ public class PdfGeneratorService {
     public static void main(String[] args) {
         Javalin app = Javalin.create(JavalinConfig::enableDevLogging).start(7070);
         app.get("/", ctx -> ctx.result("Hello World"));
+
+        /**
+         * accept HTML from a POST body.
+         */
+        app.post("/generate", ctx -> {
+            //1. inputs
+            String wordListAsHTML = ctx.body();
+            System.out.print(wordListAsHTML);
+            // or String html = ctx.formParam("html");
+            // or bodyAsBytes() or bodyAsInputStream();
+        });
     }
 }
