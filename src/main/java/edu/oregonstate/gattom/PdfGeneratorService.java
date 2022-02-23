73bad1312b42a09976ee7f6aba12787ee1477027
Michael Gatto <mgatto@vinsuite.com>
2022-02-22 20:48:59 -0700

    feat: convert html to pdf and send to client

--- src/main/java/edu/oregonstate/gattom/PdfGeneratorService.java
+++ src/main/java/edu/oregonstate/gattom/PdfGeneratorService.java
@@ -2,9 +2,15 @@ package edu.oregonstate.gattom;
 
 import io.javalin.Javalin;
 import io.javalin.core.JavalinConfig;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.xhtmlrenderer.layout.SharedContext;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+import java.io.*;
 
 public class PdfGeneratorService {
     public static void main(String[] args) {
+        //TODO use createStandalone() for Jetty-less WAR file for Heroku
         Javalin app = Javalin.create(JavalinConfig::enableDevLogging).start(7070);
         app.get("/", ctx -> ctx.result("Hello World"));
 
@@ -14,9 +20,36 @@ public class PdfGeneratorService {
         app.post("/generate", ctx -> {
             //1. inputs
             String wordListAsHTML = ctx.body();
-            System.out.print(wordListAsHTML);
             // or String html = ctx.formParam("html");
             // or bodyAsBytes() or bodyAsInputStream();
+
+            //2. Convert to PDF
+            Document document = Jsoup.parse(wordListAsHTML, "UTF-8");
+            document.outputSettings().syntax(Document.OutputSettings.Syntax.xml);
+
+            ITextRenderer renderer = new ITextRenderer();
+            renderer.setDocumentFromString(document.html());
+            renderer.layout();
+
+            // switch to using a different output stream,
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            renderer.createPDF(baos);
+
+            // then copy it into an input stream which ctx.response() can accept
+            InputStream resultStream = new ByteArrayInputStream(baos.toByteArray());
+            baos.close();
+
+            ctx.result(resultStream)
+                    .contentType("application/pdf")
+                    .header("Content-Disposition", "attachment; filename=wordlist.pdf");;
+
+
+            /*
+            try (OutputStream outputStream = new FileOutputStream(outputPdf)) {
+                SharedContext sharedContext = renderer.getSharedContext();
+                sharedContext.setPrint(true);
+                sharedContext.setInteractive(false);
+            }*/
         });
     }
 }
